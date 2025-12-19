b071b9750bd0c2225cdcd2a1aba5cc470a9e9bb9
Eray Gundesli <eray@gundesli.com>
2025-12-19 11:10:40 +0100

    [Platform] albert update models (#1212)

--- examples/albert/embeddings.php
+++ examples/albert/embeddings.php
@@ -15,7 +15,7 @@ require_once dirname(__DIR__).'/bootstrap.php';
 
 $platform = PlatformFactory::create(env('ALBERT_API_KEY'), env('ALBERT_API_URL'), http_client());
 
-$response = $platform->invoke('embeddings-small', <<<TEXT
+$response = $platform->invoke('openweight-embeddings', <<<TEXT
     Once upon a time, there was a country called Japan. It was a beautiful country with a lot of mountains and rivers.
     The people of Japan were very kind and hardworking. They loved their country very much and took care of it. The
     country was very peaceful and prosperous. The people lived happily ever after.
